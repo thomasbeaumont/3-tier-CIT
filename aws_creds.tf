c4f96fb32ca2f232b3bc3104c9794c039387770f
Thomas Beaumont <beaumont_thomas@hotmail.com>
2020-06-17 14:23:35 +0000

    AWS credentials

--- aws_creds.tf
+++ aws_creds.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "${var.aws_region}"
-  shared_credentials_file = "/mnt/visualstudio/credentials/aws_creds"
-#  profile                 = "customprofile"
+  #region     = "${var.aws_region}"
+  #shared_credentials_file = "/mnt/visualstudio/credentials/aws_creds"
+  profile                 = "customprofile"
 }
